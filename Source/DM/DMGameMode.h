61e91278c776078a94122feccb93250057510380
cheonyongjun <yes1312@naver.com>
2023-08-13 23:05:32 +0900

    init

---
+++ Source/DM/DMGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DMGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ADMGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ADMGameMode();
+};
+
+
+
